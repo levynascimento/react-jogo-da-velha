937745eb80b935f0ee8de4c201bb6802c45d026e
Levy Martins Nascimento <levymnascimento@hotmail.com>
2022-01-24 15:14:35 -0300

    inicializando o projeto

---
+++ src/components/gamePage/GamePage.jsx
@@ -0,0 +1,13 @@
+import Header from '../header/Header'
+
+function GamePage () {
+  return (
+    <div className = "container">
+      <Header />
+    </div>
+      
+    
+  )
+}
+
+export default GamePage
