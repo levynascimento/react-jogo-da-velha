25d80efa0f07f5cd6254bc80604488c404ccf37a
Levy Martins Nascimento <levymnascimento@hotmail.com>
2022-01-25 14:28:40 -0300

    Criado estados no gameoption

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,8 +1,19 @@
-import styles from './GameOption.module.css'
 
-function GameOption () {
+import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
+
+const GameIcon = ({ iconName }) => <Icon iconName = { iconName } size = "25px" />
+
+function GameOption ({ status }) {
   return (
     <div className = { styles.gameOption }>
+      {
+        status === 1 && <GameIcon iconName = "circle"  />
+      }
+      {
+        status === -1 && <GameIcon iconName = "x" />
+      }
+      
       
     </div>
   )
