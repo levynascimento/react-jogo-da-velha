047d4467da7b62ee631780fc88af8081a6f9de9d
Levy Martins Nascimento <levymnascimento@hotmail.com>
2022-02-01 12:24:49 -0300

    Criado condicional de vencedor no css

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -22,6 +22,7 @@ function Game () {
   const [gameState, setGameState] = useState(Array(9).fill(0)) 
   const [currentPlayer, setCurrentPlayer] = useState(-1)
   const [winner, setWinner] = useState(0)
+  const [winnerLine, setWinnerLine] = useState([])
 
   const handleClick = (pos) => {
     if (gameState[pos] === 0 && winner === 0) {
@@ -36,14 +37,20 @@ function Game () {
     winnerTable.forEach((line) => {
       const values = line.map ((value) => gameState[value])
       const sum = values.reduce ((sum, value) => sum + value)
-      if (sum === 3 || sum === -3) setWinner(sum / 3)
+      if (sum === 3 || sum === -3) {
+        setWinner(sum / 3)
+        setWinnerLine(line)
+      }
     })
   }
 
   const handleReset = () => {
     setGameState(Array(9).fill(0))
     setWinner(0)
+    setWinnerLine([])
   }
+
+  const verifyWinnerLine = (pos) => winnerLine.find((value) => value === pos) !== undefined
  
   useEffect(() => {
     setCurrentPlayer( currentPlayer * -1)
@@ -59,6 +66,7 @@ function Game () {
             key = {`game-option-pos-${pos} `}
             status = { value }
             onClick={() => handleClick(pos)}
+            isWinner = {verifyWinnerLine(pos)}
           />
         )
       }
